19e7ea76180c4985546a90fc6452e82014c14ccd
JoseFernandoDeLeon <dele20316@uvg.edu.gt>
2022-02-13 23:02:27 -0600

    Prelab del laboratorio #4 del curso de programación de microcontroladores de la Universidad del Valle de Guatemala

--- Prelab.s
+++ Prelab.s
@@ -1,3 +1,44 @@
+;-------------------------------------------------------------------------------
+;Encabezado
+;-------------------------------------------------------------------------------
+    
+; Archivo: Prelab_main.s
+; Dispositivo: PIC16F887
+; Autor: José Fernando de León González
+; Compilador:  pic-as (v2.30), MPLABX v5.40
+;
+; Programa: Contador binario de 4 bits utilizando push-buttons e interrupciones
+; Hardware: 
+;
+; Creado: 7/02/22
+; Última modificación: 7/02/22
+    
+PROCESSOR 16F887
 
+;-------------------------------------------------------------------------------
+;Palabras de configuración 
+;-------------------------------------------------------------------------------
+    
+; CONFIG1
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+  CONFIG  PWRTE = ON            ; Power-up Timer Enable bit (PWRT enabled)
+  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
+  CONFIG  BOREN = OFF            ; Brown Out Reset Selection bits (BOR enabled)
+  CONFIG  IESO = OFF             ; Internal External Switchover bit (Internal/External Switchover mode is enabled)
+  CONFIG  FCMEN = OFF            ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+  CONFIG  LVP = ON              ; Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
 
+; CONFIG2
+  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
 
+;-------------------------------------------------------------------------------
+;Librerías incluidas
+;-------------------------------------------------------------------------------
+  
+#include <xc.inc>
+  
+END
